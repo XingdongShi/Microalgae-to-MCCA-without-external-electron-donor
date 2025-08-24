7e112d65b22c13429114179a6bd2a1e8784e0583
Xingdong Shi <xingdong.shi@hhu.edu.cn>
2025-08-24 14:24:35 +0800

    2000 mente carlo

--- uncertainties.py
+++ uncertainties.py
@@ -110,7 +110,7 @@ def build_context_for_system(microalgae_mcca_sys, sys_label='baseline'):
 
 # %% 
 
-N_simulations_per_mode = 20 # 2000 Monte Carlo
+N_simulations_per_mode = 2000 # 2000 Monte Carlo
 
 percentiles = [0, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 1]
 
